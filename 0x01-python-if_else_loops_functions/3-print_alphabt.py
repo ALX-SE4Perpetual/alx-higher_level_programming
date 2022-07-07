0f94f86ccbe4be5bbea764a10bdf675153215ec3
Perpy-del <perpetualmeninwa@gmail.com>
2022-07-07 14:31:04 +0000

    Print ASCII in lowercase except q and e

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,11 +1,5 @@
 #!/usr/bin/python3
-
-# print ASCII alphabets in lowercase
-# except letters q and e
-# Only use one print function
-# Only use one loop in your code
-# Do not store char in a variable and do not import module
-
+# print ASCII alphabets in lowercase except letters q and e
 for letters in range(97, 123):
     if chr(letters) != 'q' and chr(letters) != 'e':
 	print("{}".format(chr(letters)), end="")
