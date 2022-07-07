5bed1588dd6224382e711be151e1ed03d39f4825
Perpy-del <perpetualmeninwa@gmail.com>
2022-07-07 21:56:06 +0000

    Fizzbuzz

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""Print the numbers of 1 to 100 separated by a space
+Print FizzBuzz for numbers which are multples of both three and five
+Print Fizz for numbers which are multiples of three
+Print Buzz for multiples of five
+Print number for the rest
+"""
+
+def fizzbuzz():
+
+    for fz in range(1, 101):
+        if fz % 3 == 0 and fz % 5 == 0:
+            print("FizzBuzz ", end="")
+        elif fz % 3 == 0:
+            print("Fizz ", end="")
+        elif fz % 5 == 0:
+            print("Buzz ", end="")
+        else:
+            print("{} ".format(number), end="")
