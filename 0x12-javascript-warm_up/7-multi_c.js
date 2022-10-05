532d8ca7b2e443f418b386694c391e09d703246f
Perpy-del <perpetualmeninwa@gmail.com>
2022-10-05 06:26:10 +0000

    print one multiple

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+if (isNaN(process.argv[2]) || process.argv[2] === undefined) {
+  console.log('Missing number of occurrences');
+} else {
+  let i = 0;
+  while (i < x) {
+    console.log('C is fun');
+    i++;
+  }
+}
