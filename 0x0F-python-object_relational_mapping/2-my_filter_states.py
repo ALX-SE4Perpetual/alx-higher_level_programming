2cb449269236b08ef5fe64441c3ea79bfed6ebcc
Perpy-del <perpetualmeninwa@gmail.com>
2022-10-10 12:06:01 -0700

    filter states

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -5,38 +5,15 @@ hbtn_0e_0_usa where name matches the argument.
 (Safe from SQL Injection)
 """
 
-if __name__ == '__main__':
-    from sys import argv
-    import MySQLdb as mysql
-    import re
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    cur = db.cursor()
 
-    if (len(argv) != 5):
-        print('Use: username, password, database name, state name')
-        exit(1)
-
-    searched = ' '.join(argv[4].split())
-
-    if (re.search('^[a-zA-Z ]+$', searched) is None):
-        print('Enter a valid name state (example: Arizona)')
-        exit(1)
-
-    try:
-        db = mysql.connect(host='localhost', port=3306, user=argv[1],
-                           passwd=argv[2], db=argv[3])
-
-    except Exception:
-        print('Failed to connect to the database')
-        exit(0)
-
-    cursor = db.cursor()
-
-    cursor.execute("SELECT * FROM states \
-                    WHERE name = '{:s}' ORDER BY id ASC;".format(searched))
-
-    result_query = cursor.fetchall()
-
-    for row in result_query:
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'"
+                 .format(sys.argv[4]))
+    rows = cur.fetchall()
+    for row in rows:
         print(row)
-
-    cursor.close()
+    cur.close()
     db.close()
