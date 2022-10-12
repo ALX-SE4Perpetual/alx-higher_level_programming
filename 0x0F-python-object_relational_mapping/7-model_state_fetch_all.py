521e52fe6e395740ec1a918e9ebe0c32dfef0638
Perpy-del <perpetualmeninwa@gmail.com>
2022-10-11 21:46:40 -0700

    Fetch all

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -11,6 +11,7 @@ if __name__ = "__main__":
 
     if (len(argv) != 4):
         print('Use username, password database_name')
+        exit(1)
 
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format
             (sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
