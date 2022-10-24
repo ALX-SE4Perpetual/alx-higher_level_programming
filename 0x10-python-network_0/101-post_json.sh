a4e305756a731cfff7d49cb0233bd156712325f0
Perpy-del <perpetualmeninwa@gmail.com>
2022-10-24 10:05:15 -0700

    post json

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Send a JSON POST request to a URL passed as the first argument, and displays the body of the response
-curl -s "${1}" -H "Content-Type: application/json" -d "$(cat "${2}")"
+curl -s "${1}" -H "Content-Type: application/json" -d "$(cat "$2")"
