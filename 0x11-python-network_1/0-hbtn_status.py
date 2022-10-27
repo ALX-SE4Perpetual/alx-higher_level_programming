145a9a47e1dd83b4d3e7c5d894515d146b827934
Perpy-del <perpetualmeninwa@gmail.com>
2022-10-27 10:33:35 -0700

    Fetch url

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """Fetch https://alx-intranet.hbtn.io/status using the package urllib"""
-import urllib.request
 
 
 if __name__ == "__main__":
+    import urllib.request
 
     with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
         html = response.read()
