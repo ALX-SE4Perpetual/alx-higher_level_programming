8035aa8356c32ed1a04e1bf01cc8a25d7b7de0b0
Perpy-del <perpetualmeninwa@gmail.com>
2022-10-30 10:37:35 -0700

    Response

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -4,11 +4,9 @@ Take in a URL, sends a request to the URL and
 displays the value of the X-Request-Id variable 
 """
 import urllib.request as urllib
-import sys
+from sys import argv
+
 
 if __name__ == "__main__":
-    url = sys.argv[1]
-
-    request = urllib.request.Request(url)
-    with urllib.request.urlopen(request) as response:
-        print(dict(response.headers).get("X-Request-Id"))
+    with urllib.urlopen(argv[1]) as response:
+        print(response.info()['X-Request-Id'])
