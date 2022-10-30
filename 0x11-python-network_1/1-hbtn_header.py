0dfa137a092d74bd6c74fbdbee276b000b8c50aa
Perpy-del <perpetualmeninwa@gmail.com>
2022-10-30 10:20:21 -0700

    Response

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -7,6 +7,8 @@ import urllib.request as urllib
 from sys import argv
 
 if __name__ == "__main__":
+    url = sys.argv[1]
+
     request = urllib.request.Request(url)
     with urllib.request.urlopen(request) as response:
         print(dict(response.headers).get("X-Request-Id"))
