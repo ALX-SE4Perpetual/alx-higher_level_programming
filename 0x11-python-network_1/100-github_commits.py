0f5d90b1cb08961cbe7c54e1dac5a77695f3739d
Perpy-del <perpetualmeninwa@gmail.com>
2023-01-17 16:24:31 +0100

    Update

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,26 +1,30 @@
 #!/usr/bin/python3
 """
-Script that lists the last 10 commits from a github repo
+Please list 10 commits (from the most recent to oldest) of the repository
+“rails” by the user “rails”
+You must use the GitHub API, here is the documentation
+https://developer.github.com/v3/repos/commits/
+Print all commits by: `<sha>: <author name>` (one by line)
 """
-from sys import argv
-import requests
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
+    from requests import get
+    from sys import argv
+
     repo = argv[1]
     owner = argv[2]
+    i = 0
 
-    response = requests.get(
-        f"https://api.github.com/repos/{owner}/{repo}/commits",
-    )
-    if response.status_code < 400:
-        commits = response.json()
-        i = 0
-        try:
-            while i < 10:
-                co = commits[i]
-                name = co.get("commit").get("author").get("name")
-                print(f"{co.get(sha)}: {name}")
-                i += 1
-        except IndexError:
-            pass
+    URL = "https://api.github.com/repos/{}/{}/commits".format(owner, repo)
+
+    response = get(URL)
+    json = response.json()
+
+    for element in json:
+        if i > 9:
+            break
+        sha = element.get('sha')
+        author = element.get('commit').get('author').get('name')
+        print("{}: {}".format(sha, author))
+        i += 1
